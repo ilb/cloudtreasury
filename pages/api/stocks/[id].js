86ad73d5f6ce9f45397aa3a78b810a8b399ed938
pivotdude <egor.corostin@gmail.com>
2023-04-11 21:20:45 +0400

    role fix

--- pages/api/stocks/[id].js
+++ pages/api/stocks/[id].js
@@ -4,6 +4,6 @@ import {createRouter} from "next-connect";
 
 export default createRouter()
   // .get(handleRequest(StockUsecases, 'read'))
-  .put(handleRequest(StockUsecases, 'update', 'access:roles_update'))
-  .delete(handleRequest(StockUsecases, 'delete', 'access:roles_delete'))
+  .put(handleRequest(StockUsecases, 'update', 'access:stocks_update'))
+  .delete(handleRequest(StockUsecases, 'delete', 'access:stocks_delete'))
   .handler()
--- pages/api/stocks/index.js
+++ pages/api/stocks/index.js
@@ -3,6 +3,6 @@ import { handleRequest } from '../../../src/core/index.mjs';
 import StockUsecases from "../../../src/usecases/StockUsecases";
 
 export default createRouter()
-  .get(handleRequest(StockUsecases, 'index', 'access:roles_read'))
-  .post(handleRequest(StockUsecases, 'create', 'access:roles_create'))
+  .get(handleRequest(StockUsecases, 'index', 'access:stocks_read'))
+  .post(handleRequest(StockUsecases, 'create', 'access:stocks_create'))
   .handler();
