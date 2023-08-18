ea559d915374bd5d37e6c3a696310f8c9d22de25
pivotdude <egor.corostin@gmail.com>
2023-08-18 12:03:49 +0400

    Revert "move heartbeat from api in page"
    This reverts commit ffa031e061e2fdbac6f5b90b444b3d4ca928cc3e.

--- pages/heartbeat.js
+++
@@ -1,8 +0,0 @@
-import { createRouter } from 'next-connect';
-import { handleRequest } from '../src/core/index.mjs';
-import HeathbeatResponse from "../src/core/responses/HeartbeatResponse.mjs";
-import HeartbeatUsecases from "../src/usecases/HeartbeatUsecases.mjs";
-
-export default createRouter()
-  .get(handleRequest(HeartbeatUsecases, 'test', [], HeathbeatResponse))
-  .handler();
