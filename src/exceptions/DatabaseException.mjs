26de4fbb79373c0526d43a34cfdaf308b888f2d9
pivotdude <egor.corostin@gmail.com>
2023-08-23 16:17:21 +0400

    add error message in DatabaseException

--- src/exceptions/DatabaseException.mjs
+++ src/exceptions/DatabaseException.mjs
@@ -1,3 +1,11 @@
-export default class DatabaseException {
+import Exception from "../core/exceptions/Exception.mjs";
+
+export default class DatabaseException extends Exception {
+  constructor(message) {
+    super();
+    this.type = 'DATABASE';
+    this.status = 500;
+    this.message = message;
+  }
 
 }
