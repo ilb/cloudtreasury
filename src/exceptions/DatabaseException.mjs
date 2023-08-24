3dc281e8548c3e1f7144d6e317d91fe434577c4a
pivotdude <egor.corostin@gmail.com>
2023-08-24 12:33:21 +0400

    update error code and message

--- src/exceptions/DatabaseException.mjs
+++ src/exceptions/DatabaseException.mjs
@@ -3,6 +3,8 @@ import Exception from "../core/exceptions/Exception.mjs";
 export default class DatabaseException extends Exception {
   constructor(message) {
     super();
+    this.type = 'CRITICAL';
+    this.status = 403;
     this.message = message;
   }
 }
