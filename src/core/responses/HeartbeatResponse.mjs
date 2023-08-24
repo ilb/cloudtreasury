de5f0c6b3a66342d3a669d90fc119ddf4580592e
Egor <82532602+pivotdude@users.noreply.github.com>
2023-08-24 19:50:29 +0400

    Revert "add heartbeat"

--- src/core/responses/HeartbeatResponse.mjs
+++
@@ -1,9 +0,0 @@
-import Response from './Response.mjs';
-
-export default class HeartbeatResponse extends Response {
-  static async build(result, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.writeHead(200);
-    res.end('OK');
-  }
-}
--- src/repositories/HeartbeatRepository.mjs
+++
@@ -1,7 +0,0 @@
-import Repository from '../core/Repository.mjs';
-
-export default class HeartbeatRepository extends Repository {
-  async test() {
-    return this.prisma.$queryRaw`select * from _prisma_migrations limit 0;`;
-  }
-}
