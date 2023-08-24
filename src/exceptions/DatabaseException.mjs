c0d72ee626d257bacf34a841d5b09fb135b18b94
pivotdude <egor.corostin@gmail.com>
2023-08-24 12:20:31 +0400

    fix response code

--- src/exceptions/DatabaseException.mjs
+++ src/exceptions/DatabaseException.mjs
@@ -3,9 +3,6 @@ import Exception from "../core/exceptions/Exception.mjs";
 export default class DatabaseException extends Exception {
   constructor(message) {
     super();
-    this.type = 'DATABASE';
-    this.status = 500;
     this.message = message;
   }
-
 }
