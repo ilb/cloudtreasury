c22cea695dd7fa62f9a12e288e64c0fdf09e4bd9
pivotdude <egor.corostin@gmail.com>
2023-09-04 16:13:50 +0400

    rename apps.cloudtreasury.stub.mfd to apps.cloudtreasury.stub.mfdEnabled

--- src/stubs/index.mjs
+++ src/stubs/index.mjs
@@ -2,7 +2,7 @@ import mfdHandlers from './mfd.mjs';
 import {setupServer} from "msw/node";
 
 const handlers = [
-  process.env["apps.cloudtreasury.stub.mfd"] ? mfdHandlers : [],
+  process.env["apps.cloudtreasury.stub.mfdEnabled"] ? mfdHandlers : [],
 ].flat();
 
 async function runServer (handlers) {
